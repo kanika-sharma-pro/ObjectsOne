2872252f05c9e16b770e90f1a030848012016ede
Kanika <kanika@macbookpro.lan>
2025-03-04 21:57:13 -0500

    some useful message reminding you of wht you did

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -6,5 +6,19 @@ public class Main {
 
     public static void main(String... args) {
         Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("This is a module-using Hello World!");    }
+        logger.info("This is a module-using Hello World!");
+        Person person1 = new Person("John", 30, 165);
+        Person person2 = new Person("Dan", 40, 170);
+        Person person3 = new Person("Sue", 30, 159);
+
+        Person[] people = new Person[10]; // an array of 10 Person objects
+        people[0] = person1;
+        people[1] = person2;
+        people[2] = person3
+
+
+
+
+
+    }
 }
---
+++ object1/src/main/java/object1/Person.java
@@ -0,0 +1,23 @@
+// Define a class
+class Person {
+    // Define Instance Variables
+    // different for each object!
+    String name;
+    int age = 0;
+    int height = 0; // units are centimeters
+
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public static void main(String[] args) {
+        // Declare and initialize an object
+        Person person1 = new Person("John", 30, 165);
+
+    /* Now you can use the Object `person1` */
+
+    }
+}
